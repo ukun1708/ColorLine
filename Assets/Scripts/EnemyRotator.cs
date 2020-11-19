5c7bf78ab39132f46fca08c312cb921d1ff6382e
Виталий Лукин <mr.lukin1990@mail.ru>
2020-11-19 16:56:39 +0300

    update

---
+++ Assets/Scripts/EnemyRotator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyRotator : MonoBehaviour
+{
+
+    public float speedRotation = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.rotation *= Quaternion.Euler(Vector3.up * speedRotation * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/TextScaler.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextScaler : MonoBehaviour
+{
+    public float speedScale = 5f;
+
+    public static TextScaler Singleton;
+    void Start()
+    {
+        Singleton = this;
+        ScalerText();
+    }
+
+    public void ScalerText()
+    {
+        StartCoroutine(TxtScaler());
+    }
+
+    IEnumerator TxtScaler()
+    {
+        RectTransform rect = GetComponent<RectTransform>();
+        Vector3 originalScale = rect.localScale;
+        Vector3 scaledScale = new Vector3(0.8f, 0.8f, 0.8f);
+
+        while (true)
+        {
+            for (float i = 0.01f; i < speedScale; i++)
+            {
+                rect.localScale = Vector3.Lerp(originalScale, scaledScale, Mathf.Min(1f, i / speedScale));
+                yield return null;
+            }
+
+            for (float i = 0.01f; i < speedScale; i++)
+            {
+                rect.localScale = Vector3.Lerp(scaledScale, originalScale, Mathf.Min(1f, i / speedScale));
+                yield return null;
+            }
+        }
+    }
+}
