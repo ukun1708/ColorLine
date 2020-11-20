3694259e8321fe4c30b343176b05fadf7f03d5a7
Виталий Лукин <mr.lukin1990@mail.ru>
2020-11-20 15:26:53 +0300

    Create Particle System

---
+++ Assets/Scripts/Flame.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flame : MonoBehaviour
+{
+   
+    void Start()
+    {
+        gameObject.GetComponent<ParticleSystem>().Stop();
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            gameObject.GetComponent<ParticleSystem>().Play();
+        }
+        else
+        {
+            gameObject.GetComponent<ParticleSystem>().Stop();
+        }
+    }
+}
